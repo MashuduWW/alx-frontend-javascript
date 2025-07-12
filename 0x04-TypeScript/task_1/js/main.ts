42e8dbb901a0ff544c03edbbff07b17880855c6d
Mashudu <62095778+MashuduWW@users.noreply.github.com>
2025-07-12 13:50:38 +0200

    Attempted tasks 1 to 4.

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,5 +4,59 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string]: any; // Allows arbitrary attributes like contract
+  [key: string]: any;                        // Allows addition of attributes like "contract"
 }
+
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+//  Function type interface
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Defining function
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+// Interface describing the class structure
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Interface describing the constructor
+interface StudentConstructorInterface {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+// Class implementing the class interface
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
